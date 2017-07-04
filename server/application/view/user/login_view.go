270155bd2231befa28e5e5a965051069db0a8abe
wolf1996 <ksgiv37@gmail.com>
2017-07-04 21:27:49 +0300

    пофиксил поля структуры логина

--- server/application/view/user/login_view.go
+++ server/application/view/user/login_view.go
@@ -3,8 +3,8 @@ package user
 import "regexp"
 
 type LoginForm struct {
-	EMail string `json:"eMail"`
-	Pass  string `json:"passWord"`
+	EMail string `json:"e_mail"`
+	Pass  string `json:"password"`
 }
 
 var mailAddressRE = regexp.MustCompile(`^([a-zA-Z0-9][-_.a-zA-Z0-9]*)(@[-_.a-zA-Z0-9]+)?$`)
